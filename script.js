cad59494e87feed99eafa13ad6befc41530bec54
pissulin <pissulin.ads@gmail.com>
2021-03-29 21:43:54 -0300

    calculo funcionando

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const res = async ()=> {
-fetch('https://mini-server-cotacao.herokuapp.com/api/v1/cotacao',
-    {
-        mode:'no-cors',
-        headers: new Headers({
-            'Content-Type': 'application/json',
-            'Accept-Charset' : 'utf-8',
+ const res = await fetch('https://mini-server-cotacao.herokuapp.com/api/v1/cotacao')
+    
+    const resu =  await res.json()
 
-        })
-    })
-    .then(res => {
-        console.log("Status "+ res.status)
+  //  const arrayResu = [...resu]
         
-    })
-        
-    //const resJSON = resp.json()
-    //console.log(resJSON)
+     document.querySelector('.dolarComercial').innerHTML = resu[0].valor
+     document.querySelector('.dolarTurismo').innerHTML = resu[1].valor
+     document.querySelector('.dolarTpax').innerHTML = resu[2].valor
+     document.querySelector('.euroComercial').innerHTML = resu[3].valor
+     document.querySelector('.dolarXeuro').innerHTML = resu[4].valor
+
+     
+    
 }
     
 res()
-    //.catch(err => console.log(`Deu ruim, o erro é: ${err}`))
+
+
+
+function converterMoedas(){
+    const moeda1 = document.getElementById('moedas1').value
+
+    let inputMoeda1 = Number(document.getElementById('moeda1').value)
+       
+    const inputMoeda2 = (document.getElementById('moeda2').value).replace(",", ".")
+
+    if(moeda1 ==="dolarComercial"){
+        const dolar = (document.querySelector('.dolarComercial').innerHTML).replace(",", ".")
+      
+        let resConversao = (inputMoeda1 * parseFloat(dolar).toFixed(2)).toFixed(2) 
+        let resConversaoString = resConversao.toString()
+
+        document.getElementById('moeda2').value = resConversaoString 
+    }
+    if(moeda1 ==="dolarTurismo"){
+        const dolar = (document.querySelector('.dolarTurismo').innerHTML).replace(",", ".")
+      
+        let resConversao = (inputMoeda1 * parseFloat(dolar).toFixed(2)).toFixed(2) 
+        let resConversaoString = resConversao.toString()
+
+        document.getElementById('moeda2').value = resConversaoString 
+    }
+    
+}
